c15ed8c0267d09af64397cd59fa109b4d122d738
franck <fde197@gmail.com>
2015-03-06 11:26:27 +0100

    update plot1 add plot2

--- plot2.R
+++ plot2.R
@@ -1,20 +1,26 @@
-plot1<- function() {
+plot2<- function() {
    # read the file
    print("Read table...")
    data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?")
-
+   
    # extract element to analyse
    print("Extract the 2 days...")
    subData <- subset(data,(as.Date(data$Date,format='%d/%m/%Y')==as.Date('2007-02-01',format='%Y-%m-%d')) | (as.Date(data$Date,format='%d/%m/%Y')==as.Date('2007-02-02',format='%Y-%m-%d')))
-
+   
+   # combine date and time in a new col
+   print("Combine date and time")
+   library(data.table)
+   subData2 <- as.data.table(subData)
+   subData3 <- subData2[, dt:=paste(subData2$Date, subData2$Time, sep=" ")]
+   
    # create a png 480 x 480
-   print("Create plot1.png")
-   png("plot1.png", width=480, height=480)
-
+   print("Create plot2.png")
+   png("plot2.png", width=480, height=480)
+   
    # histogram
    print("Make Histogram")
-   hist(subData$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
-
+   plot(strptime(subData3$dt, "%d/%m/%Y %H:%M:%S"),subData3$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+   
    # copy on file
    print("Copy on file")
    dev.off()
