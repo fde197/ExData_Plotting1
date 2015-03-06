25915cf79e8100a84458f8b0628705e910f52c34
franck <fde197@gmail.com>
2015-03-06 15:43:15 +0100

    mod plot1 and plot2; add plot3

--- plot3.R
+++ plot3.R
@@ -1,4 +1,4 @@
-plot2<- function() {
+plot3<- function() {
    # read the file
    print("Read table...")
    data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?")
@@ -17,12 +17,17 @@ plot2<- function() {
    Sys.setlocale("LC_TIME", "C")
    
    # create a png 480 x 480
-   print("Create plot2.png")
-   png("plot2.png", width=480, height=480)
+   print("Create plot3.png")
+   png("plot3.png", width=480, height=480)
    
    # histogram
    print("Make Histogram")
-   plot(strptime(subData$dt, "%d/%m/%Y %H:%M:%S"),subData$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+   plot(strptime(subData$dt, "%d/%m/%Y %H:%M:%S"), subData$Sub_metering_1, xlab="", ylab="Energy sub metering", col="black", type="l")
+   points(strptime(subData$dt, "%d/%m/%Y %H:%M:%S"), subData$Sub_metering_2, xlab="", ylab="Energy sub metering", col="red", type="l")
+   points(strptime(subData$dt, "%d/%m/%Y %H:%M:%S"), subData$Sub_metering_3, xlab="", ylab="Energy sub metering", col="blue", type="l")
+   
+   # add legend
+   legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c("black","red", "blue"), lwd=2)
    
    # copy on file
    print("Copy on file")
