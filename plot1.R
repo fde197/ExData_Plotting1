89ab026f930b493e8c90b01aca8cbba963887f3d
franck <fde197@gmail.com>
2015-03-06 10:34:42 +0100

    plot1

---
+++ plot1.R
@@ -0,0 +1,21 @@
+plot1<- function() {
+   # read the file
+   print("Read table...")
+   data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?")
+
+   # extract element to analyse
+   print("Extract the 2 days...")
+   subData <- subset(data,(as.Date(data$Date,format='%d/%m/%Y')==as.Date('2007-02-01',format='%Y-%m-%d')) | (as.Date(data$Date,format='%d/%m/%Y')==as.Date('2007-02-02',format='%Y-%m-%d')))
+
+   # create a png 480 x 480
+   print("create png")
+   png("plot1.png", width=480, height=480)
+
+   # histogram
+   print("Make Histogram")
+   hist(subData$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+
+   # copy on file
+   print("Copy on file")
+   dev.off()
+}
